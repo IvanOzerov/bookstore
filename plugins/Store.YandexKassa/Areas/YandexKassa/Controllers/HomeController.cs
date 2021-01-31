fe168329a4874f283650b173537f728313df21a3
IvanOzerov <78302786+IvanOzerov@users.noreply.github.com>
2021-01-31 15:48:04 +0300

    Add files via upload

---
+++ plugins/Store.YandexKassa/Areas/YandexKassa/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.YandexKassa.Areas.YandexKassa.Models;
+
+namespace Store.YandexKassa.Areas.YandexKassa.Controllers
+{
+    [Area("YandexKassa")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int orderId, string returnUri)
+        {
+            var model = new ExampleModel
+            {
+                OrderId = orderId,
+                ReturnUri = returnUri,
+            };
+
+            return View(model);
+        }
+
+        public IActionResult Callback(int orderId, string returnUri)
+        {
+            var model = new ExampleModel
+            {
+                OrderId = orderId,
+                ReturnUri = returnUri,
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ plugins/Store.YandexKassa/YandexKassaPaymentService.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Store.Contractors;
+using Store.Web.Contractors;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Store.YandexKassa
+{
+    public class YandexKassaPaymentService : IPaymentService, IWebContractorService
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public YandexKassaPaymentService(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        private HttpRequest Request => httpContextAccessor.HttpContext.Request;
+
+        public string Name => "YandexKassa";
+
+        public string Title => "Оплата банковской картой";
+
+        public Form FirstForm(Order order)
+        {
+            return Form.CreateFirst(Name)
+                       .AddParameter("orderId", order.Id.ToString());
+        }
+
+        public Form NextForm(int step, IReadOnlyDictionary<string, string> values)
+        {
+            if (step != 1)
+                throw new InvalidOperationException("Invalid Yandex.Kassa payment step.");
+
+            return Form.CreateLast(Name, step + 1, values);
+        }
+
+        public OrderPayment GetPayment(Form form)
+        {
+            if (form.ServiceName != Name || !form.IsFinal)
+                throw new InvalidOperationException("Invalid payment form.");
+
+            return new OrderPayment(Name, "Оплатой картой", form.Parameters);
+        }
+
+        public Uri StartSession(IReadOnlyDictionary<string, string> parameters, Uri returnUri)
+        {
+            var queryString = QueryString.Create(parameters);
+            queryString += QueryString.Create("returnUri", returnUri.ToString());
+
+            var builder = new UriBuilder(Request.Scheme, Request.Host.Host)
+            {
+                Path = "YandexKassa/",
+                Query = queryString.ToString(),
+            };
+
+            if (Request.Host.Port != null)
+                builder.Port = Request.Host.Port.Value;
+
+            return builder.Uri;
+        }
+
+        public Task<Uri> StartSessionAsync(IReadOnlyDictionary<string, string> parameters, Uri returnUri)
+        {
+            var uri = StartSession(parameters, returnUri);
+
+            return Task.FromResult(uri);
+        }
+    }
+}
